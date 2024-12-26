e5bf31197266366deb14c8bd6aaa0490f02f4290
Ademola Ademeso <desmondademeso@gmail.com>
2024-12-26 02:23:59 +0100

    feat: add initial Footer component based on Figma design

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NotFound from "./components/NotFound/NotFound";
 import NavBar from "./components/NavBar/NavBar";
 import MobileNavBar from "./components/NavBar/MobileNavBar";
 import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
 const App = () => {
   return (
     <Router>
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </Router>
   );
 };
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <footer className="h-52 bg-[#470258]"></footer>;
+};
+
+export default Footer;

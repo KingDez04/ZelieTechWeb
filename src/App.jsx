bc456ca225e0581af35e132a7b679b43d08aacd5
Ademola Ademeso <desmondademeso@gmail.com>
2024-12-26 02:19:59 +0100

    feat: add Home component and its route to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NotFound from "./components/NotFound/NotFound";
 import NavBar from "./components/NavBar/NavBar";
 import MobileNavBar from "./components/NavBar/MobileNavBar";
+import Home from "./components/Home/Home";
 const App = () => {
   return (
     <Router>
       <NavBar />
       <MobileNavBar />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,49 @@
+import banner from "../../assets/images/homeImage.png";
+import { useNavigate } from "react-router-dom";
+
+const Home = () => {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <div
+        className="relative h-screen bg-cover bg-center"
+        style={{ backgroundImage: `url(${banner})` }}
+      >
+        <div className="absolute inset-20 flex flex-col justify-center items-start">
+          <h1 className="text-white text-[37px] leading-[44.78px] md:text-6xl font-bold mb-4">
+            ZELIE TECH
+          </h1>
+          <p className="text-white text-lg md:text-[22px] leading-[26.63px] mb-8 max-w-[483px]">
+            We all have dreams we want to see become a reality. Here at ZELIE
+            TECH. We will do our best to make that idea or dream become a
+            reality no matter how absurd it sounds.
+          </p>
+          <button
+            onClick={() => {}}
+            className="text-white rounded-[30px] text-[24px] leading-[29.05px] h-[61px] w-[277px] bg-[#470258] hover:bg-[#150258] font-bold"
+          >
+            Make A Wish
+          </button>
+        </div>
+      </div>
+      <div className="text-center py-10 flex flex-col gap-5">
+        <h2 className="font-bold text-[36px] leading-[43.57px]">About Us</h2>
+        <p className="text-[27px] leading-[32.68px] font-[500px]">
+          Here at Zelie Tech, our goal is to deliver innovation tech and
+          software solutions that break barriers and redefine the words
+          “IMPOSSIBLE”.
+        </p>
+        <div>
+          <button
+            onClick={() => navigate("/services")}
+            className="text-white rounded-[30px] text-[24px] leading-[29.05px] h-[61px] w-[277px] bg-[#470258] hover:bg-[#150258] font-bold"
+          >
+            Our Services
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;

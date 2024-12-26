c338f0dc3dde075a0570aa660e1579e6274745b9
Ademola Ademeso <desmondademeso@gmail.com>
2024-12-26 20:14:11 +0100

    feat: Adjust mobile navbar layout and styling to match updated Figma designs

--- src/components/NavBar/MobileNavBar.jsx
+++ src/components/NavBar/MobileNavBar.jsx
@@ -16,11 +16,13 @@ const MobileNavBar = () => {
   };
 
   return (
-    <nav className=" m-6 relative z-50 block lg:hidden font-textFont">
+    <nav className="relative z-50 block bg-black lg:hidden px-5">
       <div className="flex items-center justify-between w-full">
-        <img src={Logo} alt="logo" className="h-[4rem]" />
+        <span className="flex items-center gap-1 font-bold text-xl text-blue-200 mx-auto">
+          <img src={Logo} alt="Zelie Logo" className="w-[40%] p-2" /> ZELIE
+        </span>
         {!nav && (
-          <div onClick={handleNav} className="cursor-pointer z-50">
+          <div onClick={handleNav} className="cursor-pointer z-50 text-white">
             <AiOutlineMenu size={35} />
           </div>
         )}
@@ -29,30 +31,36 @@ const MobileNavBar = () => {
       <div
         className={
           nav
-            ? "fixed left-0 top-0 w-[50%] h-full bg-[#ffffff] ease-in-out duration-500 z-40"
+            ? "fixed left-0 top-0 w-[50%] h-full bg-black ease-in-out duration-500 z-40"
             : "fixed top-0 left-[-100%] h-full ease-in-out duration-500"
         }
       >
         <div className="flex justify-end px-4 pt-9">
           <CgClose
             size={35}
-            className="cursor-pointer text-black z-50 "
+            className="cursor-pointer text-white z-50"
             onClick={handleNav}
           />
         </div>
 
-        <ul className="px-2 pt-[5rem] text-black space-y-8 text-[20px]">
-          <li className="py-5 px-4 hover:bg-black hover:text-white rounded-sm">
+        <div className="flex justify-center mt-14">
+          <span className="flex items-center gap-1 font-bold text-xl text-blue-200">
+            <img src={Logo} alt="Zelie Logo" className="w-[40%] p-2" />
+            ZELIE
+          </span>
+        </div>
+        <ul className="px-2 pt-[2rem] text-center text-white text-[20px]">
+          <li className="py-5 px-4 hover:bg-[#909090] hover:text-black rounded-sm">
             <Link to="/" onClick={handleCloseNav}>
               Home
             </Link>
           </li>
-          <li className="py-5 px-4 hover:bg-black hover:text-white rounded-sm">
+          <li className="py-5 px-4 hover:bg-[#909090] hover:text-black rounded-sm">
             <Link to="/services" onClick={handleCloseNav}>
               Services
             </Link>
           </li>
-          <li className="py-5 px-4 hover:bg-black hover:text-white rounded-sm">
+          <li className="py-5 px-4 hover:bg-[#909090] hover:text-black rounded-sm">
             <Link to="/contact" onClick={handleCloseNav}>
               Contact Us
             </Link>

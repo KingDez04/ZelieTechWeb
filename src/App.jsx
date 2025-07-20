2761fc4441f2e84642a5ade78644ebd8d6c3f11e
Ademola Ademeso <desmondademeso@gmail.com>
2025-07-20 17:11:43 +0100

    feat: add new routes for additional pages in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NotFound from "./components/NotFound/NotFound";
 import NavBar from "./components/NavBar/NavBar";
-import MobileNavBar from "./components/NavBar/MobileNavBar";
 import Home from "./components/Home/Home";
 // import Footer from "./components/Footer/Footer";
 import Services from "./components/Services/Services";
+import Projects from "./components/Projects/Projects";
+import Insights from "./components/Insights/Insights";
+import Launch from "./components/Launch/Launch";
+import Join from "./components/Join/Join";
 import Contact from "./components/Contact/Contact";
 const App = () => {
   return (
     <Router>
       <NavBar />
-      <MobileNavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/services" element={<Services />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/insights" element={<Insights />} />
+        <Route path="/launch" element={<Launch />} />
+        <Route path="/join-us" element={<Join />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<NotFound />} />
       </Routes>

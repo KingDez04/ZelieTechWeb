cb8401f3ab8f4cb8d68fb9060f96d412b4454b1a
Ademola Ademeso <desmondademeso@gmail.com>
2025-07-20 16:56:11 +0100

    refactor: restructure NavBar component and add responsive layout

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,43 +1,94 @@
-import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import { CgClose } from "react-icons/cg";
+import { AiOutlineMenu } from "react-icons/ai";
+import { Link } from "react-router-dom";
 import Logo from "../../assets/images/Logo.svg";
 
-const NavBar = () => {
+const MobileNavBar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav((prevNav) => !prevNav);
+  };
+
+  const handleCloseNav = () => {
+    setNav(false);
+  };
+
   return (
-    <nav
-      className="mx-auto hidden lg:flex justify-start bg-black text-white"
-      aria-label="Main Navigation"
-    >
-      <span className="flex items-center gap-1 font-bold ml-16 text-xl text-blue-200">
-        <img src={Logo} alt="Zelie Logo" className="w-[40%] p-2" />
-        ZELIE
-      </span>
-      <ul className="flex self-center space-x-3">
-        <li>
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? "underline" : "")}
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/services"
-            className={({ isActive }) => (isActive ? "underline" : "")}
-          >
-            Services
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/contact"
-            className={({ isActive }) => (isActive ? "underline" : "")}
-          >
-            Contact Us
-          </NavLink>
-        </li>
-      </ul>
+    <nav className="relative z-50 block bg-black px-5">
+      <div className="flex items-center justify-between w-full">
+        <span className="flex items-center gap-1 font-bold text-xl text-blue-200 mx-auto md:mx-0">
+          <img src={Logo} alt="Zelie Logo" className="w-[40%] p-2" /> ZELIE
+        </span>
+        {!nav && (
+          <div onClick={handleNav} className="cursor-pointer z-50 text-white">
+            <AiOutlineMenu size={35} />
+          </div>
+        )}
+      </div>
+
+      <div
+        className={
+          nav
+            ? "fixed left-0 top-0 w-[50%] md:w-[25%] h-full bg-black ease-in-out duration-500 z-40"
+            : "fixed top-0 left-[-100%] h-full ease-in-out duration-500"
+        }
+      >
+        <div className="flex justify-end px-4 pt-9">
+          <CgClose
+            size={35}
+            className="cursor-pointer text-white z-50"
+            onClick={handleNav}
+          />
+        </div>
+
+        <div className="flex justify-center mt-14">
+          <span className="flex items-center gap-1 font-bold text-xl text-blue-200">
+            <img src={Logo} alt="Zelie Logo" className="w-[40%] p-2" />
+            ZELIE
+          </span>
+        </div>
+        <ul className="px-2 pt-[2rem] text-center text-white text-[20px]">
+          <li className="py-5 px-4 hover:bg-[#909090] hover:text-black rounded-sm">
+            <Link to="/" onClick={handleCloseNav}>
+              Home
+            </Link>
+          </li>
+          <li className="py-5 px-4 hover:bg-[#909090] hover:text-black rounded-sm">
+            <Link to="/services" onClick={handleCloseNav}>
+              Services
+            </Link>
+          </li>
+          <li className="py-5 px-4 hover:bg-[#909090] hover:text-black rounded-sm">
+            <Link to="/projects" onClick={handleCloseNav}>
+              Projects
+            </Link>
+          </li>
+          <li className="py-5 px-4 hover:bg-[#909090] hover:text-black rounded-sm">
+            <Link to="/insights" onClick={handleCloseNav}>
+              Blog/Insights
+            </Link>
+          </li>
+          <li className="py-5 px-4 hover:bg-[#909090] hover:text-black rounded-sm">
+            <Link to="/launch" onClick={handleCloseNav}>
+              Launch 2025
+            </Link>
+          </li>
+          <li className="py-5 px-4 hover:bg-[#909090] hover:text-black rounded-sm">
+            <Link to="/join-us" onClick={handleCloseNav}>
+              Join Us
+            </Link>
+          </li>
+          <li className="py-5 px-4 hover:bg-[#909090] hover:text-black rounded-sm">
+            <Link to="/contact" onClick={handleCloseNav}>
+              Contact Us
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
-export default NavBar;
+
+export default MobileNavBar;
